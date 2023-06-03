eaedd794bef96d0978b9888abc77e696ca986234
Mizoko <mizoko320@gmail.com>
2023-06-03 14:24:35 +0900

    session1

--- yumemi_practice/ContentView.swift
+++ yumemi_practice/ContentView.swift
@@ -9,16 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            VStack(spacing: 0) {
+                Spacer()
+                
+                Image(systemName: "globe")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: geometry.size.width / 2)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.black, lineWidth: 2)
+                    )
+                
+                HStack(spacing: 0) {
+                    Text("Blue Label")
+                        .foregroundColor(.blue)
+                        .frame(width: geometry.size.width / 4)
+                    
+                    Text("Red Label")
+                        .foregroundColor(.red)
+                        .frame(width: geometry.size.width / 4)
+                }
+                
+                HStack(spacing: 0) {
+                    Text("Close")
+                        .foregroundColor(.blue)
+                        .frame(width: geometry.size.width / 4)
+                        .font(.system(size: 24))
+                        .padding(.top, 80)
+                    
+                    Text("Reload")
+                        .foregroundColor(.red)
+                        .frame(width: geometry.size.width / 4)
+                        .font(.system(size: 24))
+                        .padding(.top, 80)
+                }
+                
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding()
         }
-        .padding()
     }
 }
 
+// プレビュー表示設定
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
