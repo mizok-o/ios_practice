6bef1f497e72e6cf367543a3bf8ef159ef676598
Mizoko <mizoko320@gmail.com>
2023-06-03 13:24:22 +0900

    first commit

---
+++ yumemi_practice/yumemi_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  yumemi_practiceApp.swift
+//  yumemi_practice
+//
+//  Created by 溝口 耕太郎 on 2023/06/03.
+//
+
+import SwiftUI
+
+@main
+struct yumemi_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
